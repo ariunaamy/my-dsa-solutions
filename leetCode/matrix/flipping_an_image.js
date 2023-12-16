0a58492ff0982df7276099beb8d77071d861bdcf
ariunaamy <ariun.myagmar@gmail.com>
2023-12-16 10:56:10 -0500

    flipping an image

---
+++ leetCode/matrix/flipping_an_image.js
@@ -0,0 +1,53 @@
+/* 832. Flipping an Image
+Easy
+3.3K
+235
+Companies
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+To flip an image horizontally means that each row of the image is reversed.
+
+For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+For example, inverting [0,1,1] results in [1,0,0].
+ 
+
+Example 1:
+
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+Example 2:
+
+Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+*/
+
+var flipAndInvertImage = function(image) {
+    let result = []; 
+
+    const flipAndInvert = (row) => {
+        let result = []
+        for(let i=row.length-1; i>=0; i--){
+            if(row[i] === 0){
+                result.push(1); 
+            } else {
+                 result.push(0);
+            }
+        }
+        return result
+    }
+
+    for (let row of image){
+        result.push(flipAndInvert(row))
+    }
+
+    return result;
+    
+};
+
+console.log(flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]]) ) //[[1,0,0],[0,1,0],[1,1,1]]
